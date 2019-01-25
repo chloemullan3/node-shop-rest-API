0fd5b530b81cc86f4c1a7a9d572f42d8c198414a
Chloe Mullan <chloemullan3@gmail.com>
2019-01-25 17:00:38 +0000

    user login and signup completed backend

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const mongoose = require("mongoose");
 
 const productRoutes = require("./api/routes/products");
 const orderRoutes = require("./api/routes/orders");
+const userRoutes = require('./api/routes/user');
 
 mongoose.connect('mongodb://chloemullan:' + process.env.MONGO_ATLAS_PW +
   '@cluster0-shard-00-00-xjsyl.mongodb.net:27017,cluster0-shard-00-01-xjsyl.mongodb.net:27017,cluster0-shard-00-02-xjsyl.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true',
@@ -13,6 +14,7 @@ mongoose.connect('mongodb://chloemullan:' + process.env.MONGO_ATLAS_PW +
   useNewUrlParser: true
 }
 );
+mongoose.Promise = global.Promise;
 
 app.use(morgan("dev"));
 app.use('/uploads', express.static('uploads'));
@@ -35,6 +37,7 @@ app.use((req, res, next) => {
 // Routes which should handle requests
 app.use("/products", productRoutes);
 app.use("/orders", orderRoutes);
+app.use("/user", userRoutes);
 
 app.use((req, res, next) => {
   const error = new Error("Not found");
